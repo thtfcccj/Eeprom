3c8601eb058fc0a9d273af90b4adae98b7d658fa
thtfcccj <thtfcccj@qq.com>
2020-08-17 10:46:07 +0800

    Eeprom_FlashDF.c 增加连续FLASH页组合成两组全缓冲EEPROM功能，并完成相关注释,此修改未调试与测试

--- Eeprom_FlashDF.c
+++ Eeprom_FlashDF.c
@@ -3,8 +3,10 @@
       Eeprom读写模块-使用内部的Flash双页小空量空间模拟-全RAM缓冲时的实现
 
 此模块为Eeprom_FlashD.c的简化版，使用了全RAM缓冲以规避命中问题，使用条件为：
-   1. 用户的需要的EEPROM空间在一个扇区内能搞定。
-   2. 管理的两个Flash扇区均为小容量用RAM够多，以保证全缓冲区占用的RAM较小
+   1. 用户的需要的EEPROM空间在一个扇区(多页时一组连续扇区)内能搞定。
+   2. 管理的两个Flash扇区(多页时一组连续扇区)均为小容量用RAM够多，以保证全缓冲区占用的RAM较小
+
+注：默认按64k Flash容量，512byte扇区配置, EEPROM放最后，单页模式，即EEPROM大小为512byte
 ****************************************************************************/
 
 #include "Eeprom.h"
@@ -15,7 +17,6 @@
 //--------------------------宏默认值-------------------------------------
 #define _HEADER_SIZE        8      //定义数据头大小,固定为8以保证8字节对齐和校验
 
-//默认按64k容量，512byte扇区配置
 #ifndef EEPROM_WR_BACK_OV   //回写时间默认单位256ms
   #define EEPROM_WR_BACK_OV  (5 * 4)
 #endif
@@ -25,11 +26,19 @@
 #endif
 
 #ifndef EEPROM_BASE2                 //第二个EEPROM的页起始地址
+  //此值应满足(EEPROM_BASE2 - EEPROM_BASE1) >= (EEPROM_SIZE + _HEADER_SIZE)
   #define EEPROM_BASE2  (0x10000 - 0x200)  
 #endif
 
+//FLASH分页个数,可用多个连续页组成一个EEPROM_SIZE,满足：
+//((EEPROM_SIZE + _HEADER_SIZE) / EEPROM_PAGE_COUNT) 应正好对应FLASH的一页
+
+#ifndef EEPROM_PAGE_COUNT  
+  #define EEPROM_PAGE_COUNT     1     //默认只有一页
+#endif
+
 #ifndef EEPROM_SIZE
-  #define EEPROM_SIZE  (512 - 8)       //实际EEPROM的页大小,需为扇区大小-8
+  #define EEPROM_SIZE  (512 - _HEADER_SIZE)       //实际EEPROM的页大小,需为扇区大小-_HEADER_SIZE
 #endif
 
 struct _Eeprom{ //管理器
@@ -48,6 +57,8 @@ struct _Eeprom{ //管理器
 };
 
 struct _Eeprom  _Eeprom;
+
+#define  _PAGE_SIZE   ((EEPROM_SIZE + _HEADER_SIZE) / EEPROM_PAGE_COUNT) //应正好为FLASH的一页
 
 /*******************************************************************************
                            初始化与格式化及相关公共函数
@@ -98,9 +109,30 @@ static void _WrFlash(unsigned char IsPage2,
 {
   //获得绝对位置
   if(IsPage2) OffFlashAdr += EEPROM_BASE2;
-  else OffFlashAdr += EEPROM_BASE1;
-  
-  Flash_Write(OffFlashAdr, pVoid, Len);
+  else OffFlashAdr += EEPROM_BASE1;  
+  #if EEPROM_PAGE_COUNT <= 1 //一页内完成
+    Flash_Write(OffFlashAdr, pVoid, Len);
+  #else //多个FLASH页组成时，分别处理
+    unsigned short CurLen = OffFlashAdr % _PAGE_SIZE; //页内偏移
+    if((CurLen + Len) <= _PAGE_SIZE){//在一页内写完了
+      Flash_Write(OffFlashAdr, pVoid, Len);
+      return;
+    }
+    CurLen = _PAGE_SIZE - CurLen; //第一页可写入数量
+    //第一页单独写
+    const unsigned char *pPos = (const unsigned char *)pVoid;
+    Flash_Write(OffFlashAdr, pPos, CurLen);
+    Len -= CurLen;
+    pPos += CurLen;
+    OffFlashAdr += CurLen;
+    //第二页起整页写入
+    for(; Len > _PAGE_SIZE; Len -= _PAGE_SIZE, 
+                             pPos += _PAGE_SIZE, OffFlashAdr += _PAGE_SIZE){   
+      Flash_Write(OffFlashAdr, pPos, _PAGE_SIZE);
+    }
+    //最后一页单独写
+    Flash_Write(OffFlashAdr, pPos, Len);
+  #endif
 }
 
 //-----------------------格式化对应页----------------------------
@@ -109,7 +141,15 @@ static void _Format(unsigned char IsPage2)
   unsigned long FlashBase;
   if(IsPage2) FlashBase = EEPROM_BASE2;
   else FlashBase = EEPROM_BASE1;
-  Flash_ErasePage(FlashBase);
+  
+  #if EEPROM_PAGE_COUNT <= 1 //一页内完成
+    Flash_ErasePage(FlashBase);
+  #else //多个FLASH页组成时，分别格式化
+    for(unsigned char Count = EEPROM_PAGE_COUNT; Count > 0; Count--){
+      Flash_ErasePage(FlashBase);
+      FlashBase += _PAGE_SIZE;
+    }
+  #endif
 }
 
 //---------------------------初始化函数---------------------------------
@@ -163,7 +203,7 @@ static void _WrBack(void)
   //2.最新定时器累加并写入(未考虑计数到底回环到0情况)
   _Eeprom.Counter[0]++;
   _Eeprom.Counter[1] = 0 - _Eeprom.Counter[0];//取反作为校验码写入
-  _WrFlash(CurNew, 0, _Eeprom.Counter, 8);//写数据头
+  _WrFlash(CurNew, 0, _Eeprom.Counter, _HEADER_SIZE);//写数据头
   //注：忽略了对数据头写入的校验！
   
   //3.最后切换到新页以完成(注: 此时缓冲区里的数据仍然是新的并有效)
